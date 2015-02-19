0bcda9be274d2f8424a322cfb6adacddd4e53da4
unknown <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-02-19 13:37:56 +0200

    delete

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         clean:{
             all:{
-               src: ['public/!.htaccess']
+               src: ['public/*']
             }
         },
         sass: {
