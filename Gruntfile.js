faca9783cea0f3f8b09d9995b92060c01e8fbf9b
al6mina <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-02-23 11:48:16 +0200

    the latest changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,117 +1,110 @@
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-spritesmith');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-spritesmith');
 
-    grunt.initConfig({
-        clean: {
-            all:{
-               src: ['public/*']
-            }
+grunt.initConfig({
+  clean: {
+    all:{
+       src: ['public/*']
+    }
+  },
+  sprite:{
+    all: {
+      src: 'src/images/sprite/*.png',
+      dest: 'src/images/sprite.png',
+      destCss: 'src/styles/sprite.css'
+    }
+  },
+  sass: {
+    dist: {
+      options: {
+        style: 'compressed',
+        sourcemap: 'none'
+      },
+      files : {
+        'src/styles/main.css':'src/styles/main.scss'
+      }
+    }
+  },         
+  watch:{
+    options:{
+      livereload:true
+    },
+    sass:{
+      files:['src/styles/*.scss'],
+      tasks :['sass']
+    },
+    js:{
+      files:['src/js/*.js'],
+      tasks:['jshint','uglify']
+    }
+  },
+  copy: {
+    main: {
+      files:[
+        { expand: true, 
+          cwd: 'src/js/vendors/',
+          src:'*.*',
+          dest: 'public/js/vendors/',
+          filter: 'isFile'
         },
-        sprite:{
-             all: {
-                src: 'src/images/sprite/*.png',
-                dest: 'src/images/sprite.png',
-                destCss: 'src/images/sprite.css'
-          }
+        { expand: true, 
+          cwd: 'src/styles/vendors/css/',
+          src: '*.*',
+          dest: 'public/styles/vendors/css/',
+          filter: 'isFile'              
         },
-        sass: {
-            dist: {
-                options: {
-                    style: 'compressed',
-                    sourcemap: 'none'
-                },
-                files : {
-                    'src/styles/main.css':'src/styles/main.scss'
-                }
-            }
-        },         
-        watch:{
-            options:{
-                livereload:true
-            },
-            sass:{
-                files:['src/styles/*.scss'],
-                tasks :['sass']
-            },
-            js:{
-                files:['src/js/*.js'],
-                tasks:['jshint','uglify']
-            }
+        { expand: true, 
+          cwd: 'src/styles/vendors/fonts/',
+          src: '*.*',
+          dest: 'public/styles/vendors/fonts/',
+          filter: 'isFile'            
         },
-        copy: {
-              main: {
-                files:[
-                    {   expand: true, 
-                        cwd: 'src/js/vendors/',
-                        src:'*.*',
-                        dest: 'public/js/vendors/',
-                        filter: 'isFile'
-                    },
-                    {   expand: true, 
-                        cwd: 'src/styles/vendors/css/',
-                        src: '*.*',
-                        dest: 'public/styles/vendors/css/',
-                        filter: 'isFile'
-                        
-                    },
-                    {   expand: true, 
-                        cwd: 'src/styles/vendors/fonts/',
-                        src: '*.*',
-                        dest: 'public/styles/vendors/fonts/',
-                        filter: 'isFile'
-                        
-                    },
-                    {   expand: true,
-                        cwd: 'src/templates/',
-                        src:'*.*',
-                        dest: 'public/templates/',
-                        filter: 'isFile'      
-                    },
-                     {  expand: true,
-                        cwd: 'src/images/',
-                        src:'*.png',
-                        dest: 'public/images/',
-                        filter: 'isFile'      
-                    },
-                    {
-                        expand: true, flatten: true, src: ['src/*.html'], dest: 'public/', filter: 'isFile'
-                    },
-                     {
-                        expand: true, flatten: true, src: ['src/styles/main.css'], dest: 'public/styles/', filter: 'isFile'
-                    },
-                ],               
-             },       
+        { expand: true,
+          cwd: 'src/templates/',
+          src:'*.*',
+          dest: 'public/templates/',
+          filter: 'isFile'      
         },
-        concat: {
-            dist: {
-              src: ['src/js/utils.js', 'src/js/f1.js','src/js/f2.js'],
-              dest: 'src/js/screen.js'
-            },
-            
+        { expand: true,
+          cwd: 'src/images/',
+          src:'*.png',
+          dest: 'public/images/',
+          filter: 'isFile'      
         },
-       jshint: {
-            files: ['src/js/*.js'],
-            
-         },
-         uglify:{
-            main:{
-                files:{
-                    'public/js/screen.min.js':['src/js/screen.js']
-                }
-            }
-         }
+        {
+          expand: true, flatten: true, src: ['src/*.html'], dest: 'public/', filter: 'isFile'
+        },
+        {
+          expand: true, flatten: true, src: ['src/styles/main.css'], dest: 'public/styles/', filter: 'isFile'
+        },
+      ],               
+    },       
+  },
+  concat: {
+    dist: {
+      src: ['src/js/utils.js', 'src/js/f1.js','src/js/f2.js'],
+      dest: 'src/js/screen.js'
+    }        
+  },
+  jshint: {
+      files: ['src/js/*.js']        
+  },
+  uglify:{
+    main:{
+      files:{
+        'public/js/screen.min.js':['src/js/screen.js']
+      }
+    }
+  }
+});
 
-    });
-
-    
-
-    grunt.registerTask('default', ['clean', 'sprite', 'sass', 'jshint','concat','uglify','copy','watch']);
+grunt.registerTask('default', ['clean', 'sprite', 'sass', 'jshint','concat','uglify','copy','watch']);
 };
