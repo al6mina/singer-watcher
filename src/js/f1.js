cf255e978765afe535f2d4117511c4120a59631a
al6mina <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-02-23 12:11:53 +0200

    the latest

--- src/js/f1.js
+++ src/js/f1.js
@@ -3,8 +3,8 @@
 //listener for orientation change
 function doOnOrientationChange() {    
   if ((window.orientation === 0) && (window.innerWidth <= 480)) {
-     userMenu.insertBefore(podcastSection, form);
-     return;
+    userMenu.insertBefore(podcastSection, form);
+    return;
    }    
    main.insertBefore(podcastSection, social);
    return;   
