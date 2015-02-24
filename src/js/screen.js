cba52e3a8951f0701b685d6fa7ed3078be9970c9
al6mina <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-02-24 16:22:16 +0200

    change items location

--- src/js/screen.js
+++ src/js/screen.js
@@ -24,7 +24,7 @@ sw.controller ("ArtistCtrl", ['$scope', function($scope) {
 	$scope.artists = [
 		{src: 'images/artist.jpg', name: "ARTIST1"},
 		{src: 'images/artist.jpg', name: "ARTIST2"},
-		{src: 'images/artist.jpg', name: "ARTIST3"},
+		{src: 'images/artist.jpg', name: "ARTIST3with VeryLongName"},
 		{src: 'images/artist.jpg', name: "ARTIST4"},
 		{src: 'images/artist.jpg', name: "ARTIST5"},
 		{src: 'images/artist.jpg', name: "ARTIST6"},
