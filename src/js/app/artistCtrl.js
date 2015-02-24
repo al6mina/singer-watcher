e90db38544b7a2098c5bdd20796b6a9c756fa663
al6mina <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-02-24 13:31:11 +0200

    main view

---
+++ src/js/app/artistCtrl.js
@@ -0,0 +1,12 @@
+
+sw.controller ("ArtistCtrl", ['$scope', function($scope) {
+	$scope.artists = [
+		{src: 'images/artist.jpg', name: "ARTIST1"},
+		{src: 'images/artist.jpg', name: "ARTIST2"},
+		{src: 'images/artist.jpg', name: "ARTIST3"},
+		{src: 'images/artist.jpg', name: "ARTIST4"},
+		{src: 'images/artist.jpg', name: "ARTIST5"},
+		{src: 'images/artist.jpg', name: "ARTIST6"},
+		{src: 'images/artist.jpg', name: "ARTIST7"}
+	];	
+}]);
