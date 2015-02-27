90ab19cad5849c3471a39cdc825a9313290bc255
al6mina <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-02-27 16:44:39 +0200

    second view

--- src/js/showBioCtrl.js
+++ src/js/showBioCtrl.js
@@ -1,7 +1,8 @@
-
 swApp.controller('showBioCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
  	$scope.model = {
     item: $routeParams.item   
  	}; 
  	$scope.item = $routeParams.item;
+ 	$scope.src = 'images/artist.jpg';
+ 	$scope.text = "Includes 260 glyphs in font format from the Glyphicon Halflings set.Glyphicons Halflings are normally not available for free, but their creator has made them available for Bootstrap free of cost. As a thank you, we only ask that you include a link back to Glyphicons whenever possible.";
 }]);
