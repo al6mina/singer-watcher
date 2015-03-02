2eeb3abe22299fc4cfe76adcd00f7a14fa16f537
al6mina <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-03-02 11:40:09 +0200

    change bio

--- src/js/showBioCtrl.js
+++ src/js/showBioCtrl.js
@@ -1,8 +1,25 @@
-swApp.controller('showBioCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
+swApp.controller('showBioCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
  	$scope.model = {
     	item: $routeParams.item   
  	}; 
  	$scope.item = $routeParams.item;
- 	$scope.src = 'images/artist.jpg';
- 	$scope.text = "Includes 260 glyphs in font format from the Glyphicon Halflings set.Glyphicons Halflings are normally not available for free, but their creator has made them available for Bootstrap free of cost. As a thank you, we only ask that you include a link back to Glyphicons whenever possible.";
-}]);
+ 	url = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + $scope.item + '&api_key=a1b827bb5962ea81025679fd8869f5ed&format=json';
+  $http.get (url)
+        .success (function (data) {
+          if ((data.error) || (data.artist.image[2]['#text'] === '')) {
+            $scope.artistBio.src = "images/2.png";
+            $scope.artistBio.text = "Unfortunatelly, we  haven\'t  this data. Try another artist, please!";
+          } else {
+              $scope.artistBio = {
+                name: data.artist.name,
+                src: data.artist.image[3]['#text'],
+                text: data.artist.bio.content +"<br> &copy; LastFM"
+              };
+          }          
+        })
+        .error ( function (data) {
+
+        });
+        }]);        
+    
+   
--- src/js/swApp.js
+++ src/js/swApp.js
@@ -21,7 +21,12 @@ swApp.config(['$routeProvider', function ($routeProvider, $templateProvider) {
  }
  ]);
  
-  
+  swApp.filter("sanitize", ['$sce', function ($sce) {
+  return function(htmlCode){
+    return $sce.trustAsHtml(htmlCode);
+    };
+  }]);
+
  swApp.controller ('artists4CountryCtrl',['$scope','$http', function ($scope, $http) {
   var url = 'http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=Ukraine&api_key=a1b827bb5962ea81025679fd8869f5ed&format=json';
 
