777adfd803ddbb62e82b939caff51178d8fe4e08
Albina <al6mina@users.noreply.github.com>
2015-03-03 11:51:01 +0200

    Update showBioCtrl.js
    tab

--- src/js/showBioCtrl.js
+++ src/js/showBioCtrl.js
@@ -5,21 +5,20 @@ swApp.controller('showBioCtrl', ['$scope', '$routeParams', '$http', function ($s
  	$scope.item = $routeParams.item;
  	url = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + $scope.item + '&api_key=a1b827bb5962ea81025679fd8869f5ed&format=json';
   $http.get (url)
-        .success (function (data) {
-          if ((data.error) || (data.artist.image[2]['#text'] === '')) {
-            $scope.artistBio.src = "images/2.png";
-            $scope.artistBio.text = "Unfortunatelly, we  haven\'t  this data. Try another artist, please!";
-          } else {
-              $scope.artistBio = {
-                name: data.artist.name,
-                src: data.artist.image[3]['#text'],
-                text: data.artist.bio.content +"<br> &copy; LastFM"
-              };
-          }          
-        })
-        .error ( function (data) {
-
-        });
+    .success (function (data) {
+       if ((data.error) || (data.artist.image[2]['#text'] === '')) {
+         $scope.artistBio.src = "images/2.png";
+         $scope.artistBio.text = "Unfortunatelly, we  haven\'t  this data. Try another artist, please!";
+       } else {
+          $scope.artistBio = {
+            name: data.artist.name,
+            src: data.artist.image[3]['#text'],
+            text: data.artist.bio.content +"<br> &copy; LastFM"
+          };
+       }          
+     })
+     .error ( function (data) {
+     });
 }]);        
     
    
