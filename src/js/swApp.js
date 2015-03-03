34bc5ca5cac43b4694ece1ea6d715b430cf67084
al6mina <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-03-03 11:29:32 +0200

    prettifier

--- src/js/swApp.js
+++ src/js/swApp.js
@@ -2,26 +2,26 @@ var swApp = angular.module('swApp',['ngRoute']);
 
 swApp.config(['$routeProvider', function ($routeProvider, $templateProvider) { 
  	$routeProvider
- 	.when ('/', {
-    templateUrl: 'js/templates/artists4Country.html',
-    controller: 'artists4CountryCtrl'
-      })
-  .when ('/:item/bio', {
-    templateUrl: 'js/templates/artistBio.html',
-    controller: 'showBioCtrl'
-  })
-  .when ('/:item/TopSongs', {
-    templateUrl: 'js/templates/TopSongs.html',
-    controller: 'topSongsCtrl'
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
- }
- ]);
+   	.when ('/', {
+      templateUrl: 'js/templates/artists4Country.html',
+      controller: 'artists4CountryCtrl'
+    })
+    .when ('/:item/bio', {
+      templateUrl: 'js/templates/artistBio.html',
+      controller: 'showBioCtrl'
+    })
+    .when ('/:item/TopSongs', {
+      templateUrl: 'js/templates/TopSongs.html',
+      controller: 'topSongsCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+   }
+]);
  
   swApp.filter("sanitize", ['$sce', function ($sce) {
-  return function(htmlCode){
+  return function(htmlCode) {
     return $sce.trustAsHtml(htmlCode);
     };
   }]);
