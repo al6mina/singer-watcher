f9a953f63be4348800fdd4659fa6e23dff67f00f
al6mina <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-03-20 15:34:04 +0200

    twitter follow btn

--- src/js/showBioCtrl.js
+++ src/js/showBioCtrl.js
@@ -3,19 +3,17 @@ SW.swApp.controller('ShowBioCtrl', ['$scope', '$stateParams', '$http', function 
     $scope.model = {
         item: $stateParams.item
     };
-
+    var preloader = SW.utils.getPreloader();
     $('#searchForm input').val('');
     $('#autosuggestion').hide();
-    $('#artistMenu').append('<div id="preloader" class="preloader"><span class="spinner"></span></div>');
+    $('.artist-bio').append(preloader.htmlText);
     $scope.hide = true;
     $scope.item = $stateParams.item;
     var url = SW.config.BASE_URL + '?method=artist.getinfo&artist=' + $scope.item + SW.config.API_KEY;
-    $('#follow').html('<iframe src="//platform.twitter.com/widgets/follow_button.html?screen_name=madonna&lang=ua" style="width:300px; height:20px;" allowtransparency="true" frameborder="0" scrolling="no"></iframe>');
-    console.log($('#follow').html());
 
     $http.get(url)
         .success(function (data) {
-            $('#preloader').remove();
+            preloader.stop();
             $scope.artistBio = {};
             if ((data.error === 6) || (data.artist.image[2]['#text'] === '')) {
                 $scope.artistBio.src = 'images/nodata.png';
@@ -28,14 +26,11 @@ SW.swApp.controller('ShowBioCtrl', ['$scope', '$stateParams', '$http', function 
                     src: data.artist.image[3]['#text'],
                     text: data.artist.bio.content
                 };
-                var artist = SW.utils.transliterate($scope.item.toLowerCase());
-                var src = encodeURI('//platform.twitter.com/widgets/follow_button.html?screen_name=' + artist + '&lang=ua');
-                $('#follow').html('<iframe src='+src+' style="width:300px; height:20px;" allowtransparency="true" frameborder="0" scrolling="no"></iframe>');
+               
+                $('#follow').html('<iframe src="//platform.twitter.com/widgets/follow_button.html?screen_name=' + SW.utils.transliterate($scope.item.toLowerCase()) + '&lang=en&show_count=false" style="width:300px; height:20px;" allowtransparency="true" frameborder="0" scrolling="no"></iframe>');
 
             }
         });
-
-
 }]);
 
 
