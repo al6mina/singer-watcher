06ffefe3162ddf2bfdc676dd55d818ed0696de98
al6mina <Albina_Kremlovska@EPUALVIW0903.kyiv.epam.com>
2015-03-20 14:23:07 +0200

    twitter follow

--- src/js/showBioCtrl.js
+++ src/js/showBioCtrl.js
@@ -10,6 +10,8 @@ SW.swApp.controller('ShowBioCtrl', ['$scope', '$stateParams', '$http', function 
     $scope.hide = true;
     $scope.item = $stateParams.item;
     var url = SW.config.BASE_URL + '?method=artist.getinfo&artist=' + $scope.item + SW.config.API_KEY;
+    $('#follow').html('<iframe src="//platform.twitter.com/widgets/follow_button.html?screen_name=madonna&lang=ua" style="width:300px; height:20px;" allowtransparency="true" frameborder="0" scrolling="no"></iframe>');
+    console.log($('#follow').html());
 
     $http.get(url)
         .success(function (data) {
@@ -26,8 +28,14 @@ SW.swApp.controller('ShowBioCtrl', ['$scope', '$stateParams', '$http', function 
                     src: data.artist.image[3]['#text'],
                     text: data.artist.bio.content
                 };
+                var artist = SW.utils.transliterate($scope.item.toLowerCase());
+                var src = encodeURI('//platform.twitter.com/widgets/follow_button.html?screen_name=' + artist + '&lang=ua');
+                $('#follow').html('<iframe src='+src+' style="width:300px; height:20px;" allowtransparency="true" frameborder="0" scrolling="no"></iframe>');
+
             }
         });
+
+
 }]);
 
 
--- src/js/utils.js
+++ src/js/utils.js
@@ -14,5 +14,20 @@ SW.utils = {
             }
         };
         return preloader;
+    },
+transliterate : (
+    function() {
+        var
+            rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э і а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g),
+            eng = "shh sh ch cz yu ya yo zh ' y' e` i a b v g d e z y j k l m n o p r s t u f x  ".split(/ +/g);
+        return function(text, engToRus) {
+            var x;
+            for(x = 0; x < rus.length; x++) {
+                text = text.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x]);
+
+            }
+            return text.replace(/\s+/g, '');
+        };
     }
+)()
 };
